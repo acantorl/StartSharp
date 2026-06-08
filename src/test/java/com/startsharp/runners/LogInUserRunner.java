7f57e6e9450a924d4de0b986d430c2d2e968956b
adrian_cantor <acantorl@hotmail.com>
2026-06-08 12:55:13 -0500

    refactor: migrar runner a junit 5 y corregir asercion del dashboard

--- src/test/java/com/startsharp/runners/LogInUserRunner.java
+++ src/test/java/com/startsharp/runners/LogInUserRunner.java
@@ -1,17 +1,18 @@
 package com.startsharp.runners;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        plugin = {"pretty", "junit:target/junit-reports/TEST-cucumber.xml"},
-        features = "src/test/resources/features/login_user.feature",
-        glue = "com.startsharp.stepdefinitions",
-        snippets = CucumberOptions.SnippetType.CAMELCASE
-)
-
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.startsharp.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, io.cucumber.core.plugin.SerenityReporter2")
 public class LogInUserRunner {
+    // Esta clase permanece vacía
 }
