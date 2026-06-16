2ced2380aab162eb8ad63fecbe73f9355c5669a6
adrian_cantor <acantorl@hotmail.com>
2026-06-16 10:30:03 -0500

    refactor: eliminar clases ValidarLogin y ValidarLogout y migrar a WebElementQuestion nativo

--- src/test/java/com/startsharp/stepdefinitions/LogInUserStepDefinitions.java
+++ src/test/java/com/startsharp/stepdefinitions/LogInUserStepDefinitions.java
@@ -1,6 +1,5 @@
 package com.startsharp.stepdefinitions;
 
-import com.startsharp2.questions.ValidarLogin;
 import com.startsharp2.tasks.LogInUser;
 import com.startsharp2.userinterfaces.MyAccountPageUI;
 import io.cucumber.java.Before;
@@ -13,11 +12,9 @@ import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
 import net.serenitybdd.screenplay.questions.WebElementQuestion;
-import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
-import static org.hamcrest.Matchers.equalTo;
 
 public class LogInUserStepDefinitions {
 
@@ -29,8 +26,6 @@ public class LogInUserStepDefinitions {
     @Given("that the user navigates on the StartSharp page")
     public void thatTheUserNavigatesOnTheStartSharpPage() {
         OnStage.theActorCalled("Actor").attemptsTo(Open.url("https://demo.serenity.is/"));
-
-
     }
 
     @When("he logs with credentials {string} and {string}")
@@ -38,27 +33,15 @@ public class LogInUserStepDefinitions {
         OnStage.theActorInTheSpotlight().attemptsTo(
                 LogInUser.conCredenciales(usuario, password)
         );
-
-
     }
 
     @Then("he should see a message related to {string}")
     public void heShouldSeeAMessageRelatedTo(String expectedMessage) {
-        GivenWhenThen.seeThat(
-                WebElementQuestion.the(MyAccountPageUI.DASHBOARD_MESSAGE),
-                WebElementStateMatchers.containsText("Tablero")
-
-
-        );
-
-
+        // El actor ejecuta la aserción nativa esperando dinámicamente el texto del Feature
         theActorInTheSpotlight().should(
                 seeThat("El mensaje visible en el Dashboard",
-                        ValidarLogin.elMensajeDeDashboard(), equalTo(expectedMessage))
+                        WebElementQuestion.the(MyAccountPageUI.DASHBOARD_MESSAGE),
+                        WebElementStateMatchers.containsText(expectedMessage))
         );
-
-
     }
-
-
 }
